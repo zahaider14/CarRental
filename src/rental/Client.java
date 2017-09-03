eda3d87f2c71a7fcde0bd74507c1e64d2ac63341
Jérôme <macadamotion@yahoo.fr>
2017-09-03 17:01:12 +0200

    Add Client class

--- src/rental/Client.java
+++ src/rental/Client.java
@@ -8,8 +8,33 @@ public class Client {
   private String name;
   private int age;
 
+  /**
+  * Creates a client
+  * @param name the name of the client
+  * @param age his/her age
+  */
+  public Client(String n, int a) {
+    this.name = n;
+    this.age = a;
+  }
 
+  /**
+  * Gives the client's name
+  * @return the client's name
+  */
+  public String getName() {
+    return this.name;
+  }
 
+  /**
+  * Gives the client's age
+  * @return the age
+  */
+  public int getAge() {
+    return this.age;
+  }
 
   
+
+
 }
