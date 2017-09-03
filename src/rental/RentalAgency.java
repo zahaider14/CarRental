4d4731f3aed056ea4671040eda47ae9da77bceab
Jérôme <macadamotion@yahoo.fr>
2017-09-03 23:53:09 +0200

    Beginning of RentalAgency class

---
+++ src/rental/RentalAgency.java
@@ -0,0 +1,28 @@
+package rental;
+
+
+import rental.vehicles.*;
+import rental.criterion.*;
+
+
+/**
+* A class representing the rental agency */
+public class RentalAgency {
+
+  /** List of vehicles the agency owns */
+  protected List<Vehicle> theVehicles;
+
+  /** List of clients and the vehicles they are renting.
+  * At most, one vehicle rented by client.
+  */
+  protected Map<Client,Vehicle> rentedVehicles;
+
+  /** constructor of the rental agency */
+  public RentalAgency {
+
+
+
+
+    
+  }
+}
